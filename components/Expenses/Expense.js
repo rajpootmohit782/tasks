c86d81cc65097c885994a6a17cd31648e82467a9
rajpootmohit782 <84062871+rajpootmohit782@users.noreply.github.com>
2022-12-28 15:25:08 +0530

    t11 code

--- components/Expenses/Expense.js
+++ components/Expenses/Expense.js
@@ -1,30 +1,36 @@
-import React from 'react';
-
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
-import ExpenseFilter from './ExpenseFilter';
-
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
+import "./Expenses.css";
+ 
 const Expenses = (props) => {
-  //console.log(props.items)
-  const filterChangeHandler =()=>{
-   
-  }
+  const [filteredYear, setFilteredYear] = useState("2021");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpnses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+ 
   return (
-    <Card className="expenses">
-      <ExpenseFilter onChangeFilter = {filterChangeHandler} />
-      {
-        props.items.map( expense =>
-          
-           <ExpenseItem 
-           key = {expense.id}
-           title= {expense.title} 
-           amount = {expense.amount} 
-           date= {expense.date}/>)
-      }
-     
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpnses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
-}
-
+};
+ 
 export default Expenses;
--- components/Expenses/ExpenseFilter.js
+++ components/Expenses/ExpenseFilter.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const ExpenseFilter = () => {
+const ExpenseFilter = (props) => {
+    const dropdownChangeHandler = (event) =>{
+        props.onChangeFilter(event.target.value)
+    }
   return (
-    <div>ExpenseFilter</div>
+    <div className='expenses-filter'>
+        <div className='expense-filter_control'>
+            <label>Filter by year</label>
+            <select value={props.selected} onChange= {dropdownChangeHandler}>
+                <option value = '2022'>2022</option>
+                <option value = '2021'>2021</option>
+                <option value = '2020'>2020</option>
+                <option value = '2019'>2019</option>
+            </select>
+        </div>
+    </div>
   )
 }
 
